c5f65dc213a91aec358a244a520a74029124d0a3
mohammedrakib944 <mohammedrakib944@gmail.com>
2023-10-05 15:42:06 +0600

    MEtadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/layouts/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import siteMetadata from "@/utils/siteMetaData";
 
 const roboto = Roboto({
   weight: ["400", "500", "700", "900"],
@@ -9,13 +10,34 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://www.myselfrakib.com"),
+  metadataBase: new URL(siteMetadata.siteUrl),
   title: {
-    default: "Rakib | Web Developer",
-    template: `%s | Rakib | Web Developer`,
+    template: `%s | ${siteMetadata.title}`,
+    default: siteMetadata.title,
   },
-  description:
-    "Full Stack web developer, MERN stack developer, React.js Developer",
+  description: siteMetadata.description,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: siteMetadata.siteUrl,
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+  },
 };
 
 export default function RootLayout({
---
+++ src/utils/siteMetaData.ts
@@ -0,0 +1,13 @@
+const siteMetadata = {
+  title: "Rakib | Web Developer",
+  author: "Md. Rakibuzzaman",
+  description:
+    "Full Stack web developer | MERN stack developer | React.js Developer | JavaScript Expert",
+  siteUrl: "https://www.myselfrakib.com",
+  siteLogo: "/rakib.jpg",
+  email: "mohammedraib944@gmail.com",
+  github: "https://github.com/mohammedrakib944",
+  facebook: "https://www.facebook.com/md.rakib.38",
+  socialBanner: "/myselfrakib.png",
+};
+export default siteMetadata;
